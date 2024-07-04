58ca52ededa141b434bda14400747d09f9dd329b
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-07-04 22:54:59 +0600

    module 27 complete

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,66 +1,118 @@
+import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
+import { NavLink } from "react-router-dom";
 
-const adminPaths2 = [
-    {
-        name: "Dashboard",
-        path: '/admin/dashboard',
-        Element: <AdminDashboard />
-    },
-    {
-        name: "User Management",
-        children: [
-            {
-                name: "Create Admin",
-                path: '/admin/create-admin',
-                Element: <CreateAdmin />
-            },
-            {
-                name: "Create Faculty",
-                path: '/admin/create-faculty',
-                Element: <CreateFaculty />
-            },
-            {
-                name: "Create Student",
-                path: '/admin/create-student',
-                Element: <CreateStudent />
-            },
-        ]
-    },
-    {
-        name: "Course Management",
-        children: [
-            {
-                name: "Offered Course",
-                path: '/admin/offered-course',
-                Element: <CreateAdmin />
-            },
-        ]
-    },
-]
+type TRoute = {
+  path: string;
+  element: ReactNode;
+};
 
+type TSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
+};
 
-export const adminPaths = [
-    {
-        index: true,
-        element: <AdminDashboard />
-    },
-    {
-        path: 'dashboard',
-        element: <AdminDashboard />
-    },
-    {
-        path: 'create-admin',
-        element: <CreateAdmin />
-    },
-    {
-        path: 'create-faculty',
-        element: <CreateFaculty />
-    },
-    {
-        path: 'create-student',
-        element: <CreateStudent />
-    },
+const adminPaths = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+      {
+        name: "Create Member",
+        path: "create-member",
+        element: <CreateStudent />,
+      },
+    ],
+  },
 ];
+
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+    return acc;
+  },
+  []
+);
+
+// programmatical way
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+// Hard Coded way
+// export const adminPaths = [
+//   {
+//     index: true,
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+// ];
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,36 +2,36 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import { adminPaths } from "./admin.routes";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
-    {
-        path: '/admin',
-        element: <App />,
-        children: adminPaths,
-    },
-    {
-        path: '/faculty',
-        element: <App />,
-        children: adminPaths,
-    },
-    {
-        path: '/student',
-        element: <App />,
-        children: adminPaths,
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/register',
-        element: <Register />
-    },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
 export default router;
